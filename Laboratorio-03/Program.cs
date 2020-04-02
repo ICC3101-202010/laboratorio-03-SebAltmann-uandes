e332913bcf45766853397c9e53010beb20b904f3
Sebastián Altmann <saaltmann@miuandes.cl>
2020-04-02 16:15:46 -0300

    Project file created.

---
+++ Laboratorio-03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
