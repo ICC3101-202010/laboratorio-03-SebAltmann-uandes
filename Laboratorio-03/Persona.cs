bf075aec73eac1ea242c5d90ee23276b7e8cf3c2
Sebastián Altmann <saaltmann@miuandes.cl>
2020-04-03 17:36:43 -0300

    Added inital attributes to class: Persona

--- Laboratorio-03/Persona.cs
+++ Laboratorio-03/Persona.cs
@@ -3,8 +3,19 @@ namespace Laboratorio03
 {
     public class Persona
     {
-        public Persona()
+        private string RUT;
+        private string nombre;
+        private string apellido;
+        private string nacimiento;
+        private string nacionalidad;
+
+        public Persona(String RUT, String nombre, String apellido, String nacimiento, String nacionalidad)
         {
+            this.RUT = RUT;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nacimiento = nacimiento;
+            this.nacionalidad = nacionalidad;
         }
     }
 }
