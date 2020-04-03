ca33f7119bd0583c4ca9ed320a7937b176b14778
Sebastián Altmann <saaltmann@miuandes.cl>
2020-04-03 18:27:09 -0300

    Added initial attributes to class: Producto

--- Laboratorio-03/Producto.cs
+++ Laboratorio-03/Producto.cs
@@ -3,8 +3,17 @@ namespace Laboratorio03
 {
     public class Producto
     {
-        public Producto()
+        private string nombre;
+        private int precio;
+        private string marca;
+        private int stock;
+
+        public Producto(String nombre, int precio, String marca, int stock)
         {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.marca = marca;
+            this.stock = stock;
         }
     }
 }
